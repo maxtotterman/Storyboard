a0f84d832840154507fcc1684e24f34cc17647ee
Max Tötterman <max.philip.totterman@gmail.com>
2015-10-13 19:33:52 +0200

    Removed comments

--- src/main/java/se/kebnekaise/rest/config/filter/AuthenticationFilter.java
+++ src/main/java/se/kebnekaise/rest/config/filter/AuthenticationFilter.java
@@ -25,21 +25,14 @@ public class AuthenticationFilter implements ContainerRequestFilter
 
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
-
-		// Get the HTTP Authorization header from the request
 		String authorizationHeader =
 				requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
-
-		// Check if the HTTP Authorization header is present and formatted correctly
 		if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
 			throw new NotAuthorizedException("Authorization header must be provided");
 		}
-
-		// Extract the token from the HTTP Authorization header
 		String token = authorizationHeader.substring("Bearer".length()).trim();
 
 		try {
-			// Validate the token
 			validateToken(token);
 		}
 		catch (Exception e) {
@@ -49,7 +42,6 @@ public class AuthenticationFilter implements ContainerRequestFilter
 	}
 
 	private void validateToken(String token) throws Exception {
-		// Check if it was issued by the server and if it's not expired
 		bean.checkToken(token);
 	}
 }
